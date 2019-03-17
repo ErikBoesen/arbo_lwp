2085ef97d2bd1567f07bcc7223afda322989a2ac
ErikBoesen <me@erikboesen.com>
2019-03-17 12:12:38 -0400

    Copy code from vogella

---
+++ app/src/main/java/com/erikboesen/arbolwp/MyPoint.java
@@ -0,0 +1,13 @@
+package com.erikboesen.arbolwp;
+
+public class MyPoint {
+    public String text;
+    public float x;
+    public float y;
+
+    public MyPoint(String text, float x, float y) {
+        this.text = text;
+        this.x = x;
+        this.y = y;
+    }
+}
